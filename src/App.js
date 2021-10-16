711ea88b9cb5b9da405927e11cb0ac17f95911f4
Yuki Kitayama <yk2797@columbia.edu>
2021-10-16 16:51:27 -0600

    developing dark mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import Navigation from './components/Navigation';
+import { useState, useMemo } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { teal, amber } from "@mui/material/colors";
+import Navigation from "./components/Navigation";
+
+const getDesignTokens = (mode) => ({
+  palette: {
+    mode,
+    ...(mode === "light"
+      ? {
+          primary: {
+            main: teal[200],
+          },
+          secondary: {
+            main: amber[200],
+          },
+        }
+      : {
+          primary: {
+            main: teal[200],
+          },
+          secondary: {
+            main: amber[200],
+          },
+        }),
+  },
+});
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const colorMode = useMemo(
+    () => ({
+      // The dark mode switch would invoke this method
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Navigation />
-      <p>Hello</p>
-    </div>
+      <ul>
+        <li>Add Dark mode to toggle</li>
+        <li>Show menu is navigation bar in a wide screen</li>
+        <li>But hide in menu icon in a narrow screen</li>
+        <li>https://mui.com/customization/dark-mode/</li>
+      </ul>
+      <div>{theme.palette.mode} mode</div>
+    </ThemeProvider>
   );
 }
 
