8edf2fcc7cb2731acee6bf7f0930a14f6198ac57
Yuki Kitayama <yk2797@columbia.edu>
2021-10-23 16:02:26 -0600

    added states to all the input in the new expense component to extract user input, and improve the styly of the new expens component

--- src/pages/ExpenseNewItem.js
+++ src/pages/ExpenseNewItem.js
@@ -12,66 +12,150 @@ import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
 import Button from "@mui/material/Button";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import Grid from "@mui/material/Grid";
 
 const ExpenseNewItem = () => {
-  const [value, setValue] = useState(new Date("2021-10-21T16:40:00"));
+  const [date, setDate] = useState(new Date());
+  const [item, setItem] = useState("grocery");
   const [type, setType] = useState("expense");
+  const [amount, setAmount] = useState(0);
+  const [place, setPlace] = useState("");
+  const [memo, setMemo] = useState("");
 
-  const handleChange = (newValue) => {
-    setValue(newValue);
+  const dateChangeHandler = (newValue) => {
+    setDate(newValue);
+  };
+
+  const itemChangeHandler = (event) => {
+    setItem(event.target.value);
   };
 
   const typeChangeHandler = (event) => {
     setType(event.target.value);
   };
 
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const placeChangeHandler = (event) => {
+    setPlace(event.target.value);
+  };
+
+  const memoChangeHandler = (event) => {
+    setMemo(event.target.value);
+  };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
-    console.log(event);
+    console.log(
+      `Date: ${date.toISOString()}, Item: ${item}, Type: ${type}, Amount: ${amount}, Place: ${place}, Memo: ${memo}`
+    );
   };
 
   return (
     <Fragment>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <form onSubmit={formSubmissionHandler}>
-          <Card sx={{ width: { md: "70%" }, mt: 3 }}>
-            <CardHeader title="Add New Item" />
-            <CardContent>
-              <Stack spacing={1}>
-                <DesktopDatePicker
-                  label="Date"
-                  inputFormat="MM/dd/yyyy"
-                  value={value}
-                  onChange={handleChange}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-                <TextField label="Account" variant="outlined" />
-                <Select value={type} label="Type" onChange={typeChangeHandler}>
-                  <MenuItem value="expense">Expense</MenuItem>
-                  <MenuItem value="income">Income</MenuItem>
-                </Select>
-                <TextField label="Amount" variant="outlined" />
-                <TextField label="Place" variant="outlined" />
-                <TextField label="Memo" variant="outlined" />
-              </Stack>
-            </CardContent>
-            <CardActions>
-              <Stack direction="row" spacing={2}>
-                <Button variant="contained">Submit</Button>
-                <Button
-                  component={Link}
-                  to={"/expense"}
-                  variant="contained"
-                  color="error"
-                >
-                  Cancel
-                </Button>
-              </Stack>
-            </CardActions>
-          </Card>
-        </form>
-      </LocalizationProvider>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item sx={{ width: { xs: "100%", md: "70%" }, mt: 3 }}>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <form onSubmit={formSubmissionHandler}>
+              <Card>
+                <CardHeader title="Add New Item" />
+                <CardContent>
+                  <Stack spacing={1}>
+                    <DesktopDatePicker
+                      label="Date *"
+                      inputFormat="MM/dd/yyyy"
+                      value={date}
+                      onChange={dateChangeHandler}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          helperText={params?.inputProps?.placeholder}
+                        />
+                      )}
+                    />
+                    <FormControl fullWidth>
+                      <InputLabel id="item-label">Item *</InputLabel>
+                      <Select
+                        labelId="item-label"
+                        value={item}
+                        label="Item *"
+                        onChange={itemChangeHandler}
+                      >
+                        <MenuItem value="grocery">Grocery</MenuItem>
+                        <MenuItem value="lunch">Lunch</MenuItem>
+                        <MenuItem value="dinner">Dinner</MenuItem>
+                      </Select>
+                      <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                    <FormControl>
+                      <InputLabel id="type-label">Type *</InputLabel>
+                      <Select
+                        labelId="type-label"
+                        value={type}
+                        label="Type *"
+                        onChange={typeChangeHandler}
+                      >
+                        <MenuItem value="expense">Expense</MenuItem>
+                        <MenuItem value="income">Income</MenuItem>
+                      </Select>
+                      <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                    <TextField
+                      label="Amount"
+                      variant="outlined"
+                      required
+                      type="number"
+                      helperText="Required"
+                      defaultValue={amount}
+                      onChange={amountChangeHandler}
+                    />
+                    <TextField
+                      label="Place"
+                      variant="outlined"
+                      required
+                      helperText="Required"
+                      defaultValue={place}
+                      onChange={placeChangeHandler}
+                    />
+                    <TextField
+                      label="Memo"
+                      variant="outlined"
+                      defaultValue={memo}
+                      onChange={memoChangeHandler}
+                    />
+                  </Stack>
+                </CardContent>
+                <CardActions>
+                  <Stack direction="row" spacing={2}>
+                    <Button variant="contained" type="submit">
+                      Submit
+                    </Button>
+                    <Button
+                      component={Link}
+                      to={"/expense"}
+                      variant="contained"
+                      color="error"
+                    >
+                      Cancel
+                    </Button>
+                  </Stack>
+                </CardActions>
+              </Card>
+            </form>
+          </LocalizationProvider>
+        </Grid>
+      </Grid>
     </Fragment>
   );
 };
