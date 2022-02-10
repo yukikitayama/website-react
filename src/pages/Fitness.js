957e897f7244f5d9a830a267172b2642c2e26f90
Yuki Kitayama <yk2797@columbia.edu>
2022-02-09 21:29:07 -0700

    built daily graph for Fitness components and connected API

--- src/pages/Fitness.js
+++ src/pages/Fitness.js
@@ -1,11 +1,31 @@
 import { Fragment } from "react";
+import { Grid } from "@mui/material";
 
-import Sleep from "../components/Sleep";
+import FitnessDaily from '../components/FitnessDaily';
 
 const Fitness = () => {
   return (
     <Fragment>
-      <Sleep />
+      <Grid container spacing={2} pt={2} pb={10}>
+        <Grid item xs={12} md={6}>
+          <FitnessDaily title="Sleep" data="sleep" target={420} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <FitnessDaily title="Deep Sleep" data="deep-sleep" target={0.18} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <FitnessDaily title="Water Intake" data="water" target={2000} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <FitnessDaily title="Steps" data="steps" target={8000} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <FitnessDaily title="Weight" data="weight" target={60} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <FitnessDaily title="Calories Burn" data="calories" target={2500} />
+        </Grid>
+      </Grid>
     </Fragment>
   );
 };
